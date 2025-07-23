82fb5fffb18fe99d550bab1005f35a819e0d6913
EnedesIF <2148722@etfbsb.edu.br>
2025-07-23 19:39:32 -0300

    Delete enedes-php-system directory

--- enedes-php-system/api/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-function verificarLogin($usuario, $senha) {
-    $usuariosValidos = [
-        "admin" => "admin123",
-        "user" => "user123"
-    ];
-    return isset($usuariosValidos[$usuario]) && $usuariosValidos[$usuario] === $senha;
-}
-?>
